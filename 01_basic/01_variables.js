857e7a7e400a6b31a117abe0880d7e9ddf2087a5
Krishna Murari <165884658+krishnamuraricse@users.noreply.github.com>
2024-04-18 13:25:42 +0000

    Day 01

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "krishnamurari@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur" 
+let accountstate
+//  noaccountId = 23456
+accountEmail = "krishnamuraricse@gmail.com"
+accountPassword = "345679"
+accountCity="Patna"
+console.log(accountCity);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
+/*
+Prefer not to use Var bcz of issue in block scope and functional scope
+*/
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,2 @@
+"use strict" //treat all JS code as a newer version
+console.log(typeof"Krishna");
